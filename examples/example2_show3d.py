03ccba2937b40a571f4e1a63214d1513984a64f3
Robertino17 <141501831+Robertino17@users.noreply.github.com>
2025-03-10 06:52:41 +0300

    Update example2_show3d.py

--- examples/example2_show3d.py
+++ examples/example2_show3d.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #make the array with some values
 array = np.random.randint(0, 256, size=(20, 20, 20, 20), dtype=np.uint8)
 
-t = tenvis.Tensor(array, copy=False)
+t = tenvis.Tensor(array)
 
-#lets see a slice along the axis2 with index = 7 with a log scale and without colorbar
-t.show_3d(2, 7, log=True, colorbar=False)
+#lets see a slice along the axis2 with index = 7 with a log scale
+t.show_3d(2, 7, log=True)
